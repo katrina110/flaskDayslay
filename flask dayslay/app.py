e90f19b8930541376e69d1988da8c82583cdb9c6
Rvyvid <148046755+Rvyvid@users.noreply.github.com>
2024-01-08 11:37:15 -0800

    Update app.py

--- flask dayslay/app.py
+++ flask dayslay/app.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
