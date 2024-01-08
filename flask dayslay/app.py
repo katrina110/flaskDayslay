e3292a9f08c11a1564db3ca39dd1e662f9263ef8
katrina110 <katrinacortez110@gmail.com>
2024-01-08 11:24:31 +0800

    asd
    asd

--- flask dayslay/app.py
+++ flask dayslay/app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 
 if __name__ == "__main__":
